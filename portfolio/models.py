011127f9b574a326ac05d61568591b4d4fdd0585
Xavier Rochard <xrochard@gmail.com>
2021-09-19 00:25:35 +0200

    media settings

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100, default="Empty title")
-    description = models.CharField(max_length=250, default="Empty description")
+    description = models.CharField(max_length=250, default="")
     url = models.URLField(blank=True)
     # MEDIA_ROOT default location is <project_root>/media/
     image = models.ImageField(
