dd33f86572217a554c92c0527feb8adfcdba40c3
Xavier Rochard <xrochard@gmail.com>
2021-09-29 23:52:51 +0200

    adding home view test

--- portfolio/tests/test_views.py
+++ portfolio/tests/test_views.py
@@ -1,6 +1,8 @@
+from django.http import response
 from portfolio.models import Project
 from django.test import TestCase
 from portfolio import views
+from django.urls import reverse
 
 
 class HomePageTest(TestCase):
@@ -20,3 +22,16 @@ class ViewsTest(TestCase):
     def test_all_projects_gets_all_projects(self):
         result = views.all_projects()
         self.assertEqual(2, len(result))
+
+    def test_home_context_holds_all_projects(self):
+        expected_titles = ["project_1", "project_2"]
+
+        # indicate here the view that is tested
+        response = self.client.get(reverse("home"))
+
+        context_projects = response.context["projects"]
+        self.assertEqual(len(expected_titles), len(context_projects))
+        actual_titles = []
+        for project in context_projects:
+            actual_titles.append(project.title)
+        self.assertEqual(set(expected_titles), set(actual_titles))
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Project
 
 
-def home(request):
-    return render(request, "home.html")
-
-
 def all_projects():
     return Project.objects.all()  # pylint: disable=no-member
+
+
+def home(request):
+    projects = all_projects()
+    return render(request, "home.html", {"projects": projects})
