caa8359f1befaf3fe879853abaca14b66ec6ce1d
Xavier Rochard <xrochard@gmail.com>
2021-10-29 00:07:05 +0200

    deletion of he code from blog app (service)

--- blog/models.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-from django.db import models
-
-
-class Blog(models.Model):
-    title = models.CharField(max_length=100, default="Empty title")
-    date = models.DateField(default=date.today)
-    text = models.CharField(max_length=1000, default="")
