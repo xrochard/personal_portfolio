47d93943ad6dba13bb7af77330d7bd2b3a08875d
Xavier Rochard <xrochard@gmail.com>
2021-10-31 00:50:06 +0200

    blog: url test + code

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from blog import views
+from django.urls import path
+from blog import views
 
-# urlpatterns = [
-#     path("", views.all_blogs, name="all"),
-# ]
+urlpatterns = [
+    path("", views.all_blogs, name="all"),
+]
