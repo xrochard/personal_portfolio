600535cc82fd098a67e30779cb080b1b6abbe1c6
Xavier Rochard <xrochard@gmail.com>
2021-10-31 17:24:45 +0100

    blog: subtitle integration test

--- blog/tests/test_templates.py
+++ blog/tests/test_templates.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from django.test import TestCase
+from blog.models import Blog
 
 
 class BlogsTemplateTests(TestCase):
@@ -16,6 +17,17 @@ class BlogsTemplateTests(TestCase):
         h2_tags = self.soup.find_all("h2")
         self.assertEqual(1, len(h2_tags))
         self.assertEqual("Xavier has written 0 blogs.", h2_tags[0].contents[0])
+
+
+class BlogsTemplateWithOneBlogTests(TestCase):
+    fixtures = ["one_blog.json"]
+
+    def test_show_one_as_the_number_of_blogs(self):
+        response = self.client.get("/blog/")
+        soup = BeautifulSoup(response.content, features="html.parser")
+        h2_tags = soup.find_all("h2")
+        self.assertEqual(1, len(h2_tags))
+        self.assertEqual("Xavier has written 1 blogs.", h2_tags[0].contents[0])
 
 
 # to run the tests on command line
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from blog.models import Blog
+
+
+def blog_model_full_query():
+    blog_entries = []
+    for blog_entry in Blog.objects.all():  # pylint: disable=no-member
+        blog_entries.append(
+            {
+                "title": blog_entry.title,
+                "text": blog_entry.text,
+            }
+        )
+    return blog_entries
+
+
+def from_blog_model_to_blogs():
+    return blog_model_full_query()
 
 
 def blogs(request):
-    return render(request, "blogs.html")
+    blog_entries = from_blog_model_to_blogs()
+    number = len(blog_entries)
+    return render(request, "blogs.html", {"number": number, "blogs": blog_entries})
