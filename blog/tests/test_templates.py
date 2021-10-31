dcc90d5c0bf42997659a70d050d7626940405556
Xavier Rochard <xrochard@gmail.com>
2021-10-31 16:06:30 +0100

    add subtitle to blogs.html

--- blog/tests/test_templates.py
+++ blog/tests/test_templates.py
@@ -3,12 +3,19 @@ from django.test import TestCase
 
 
 class BlogsTemplateTests(TestCase):
-    def test_holds_Blog_title(self):
+    def setUp(self):
         response = self.client.get("/blog/")
-        soup = BeautifulSoup(response.content, features="html.parser")
-        h1_tags = soup.find_all("h1")
+        self.soup = BeautifulSoup(response.content, features="html.parser")
+
+    def test_holds_blog_title(self):
+        h1_tags = self.soup.find_all("h1")
         self.assertEqual(1, len(h1_tags))
         self.assertEqual("Blog", h1_tags[0].contents[0])
+
+    def test_show_the_number_of_blogs(self):
+        h2_tags = self.soup.find_all("h2")
+        self.assertEqual(1, len(h2_tags))
+        self.assertEqual("Xavier has written 0 blogs.", h2_tags[0].contents[0])
 
 
 # to run the tests on command line
